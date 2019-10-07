4e4588d3a24fbe0cbdcfef4bfaf8b81f811e6e70
Charles Tompkins <ctompkins32@gmail.com>
2019-10-06 20:26:56 -0500

    Create map_app1.py

---
+++ map_function/map_app1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# author: Mosh Hamadani
+# date: 11/15/2018
+# modified: 10/06/2019
+
+items = [
+        ("Product1", 10),
+        ("Product2", 9),
+        ("Product3", 12)
+]
+
+prices = prices = list(map(lambda item: item[1], items))
+print(prices)
